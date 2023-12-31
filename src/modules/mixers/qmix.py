a0087075681a82d9e3afee1a4a0cd370152d06a2
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2023-12-31 02:48:26 +0200

    Update qmix.py

--- src/modules/mixers/qmix.py
+++ src/modules/mixers/qmix.py
@@ -43,13 +43,13 @@ class QMixer(nn.Module):
         states = states.reshape(-1, self.state_dim)
         agent_qs = agent_qs.view(-1, 1, self.n_agents)
         # First layer
-        w1 = th.abs(self.hyper_w_1(states))
+        w1 = self.hyper_w_1(states)
         b1 = self.hyper_b_1(states)
         w1 = w1.view(-1, self.n_agents, self.embed_dim)
         b1 = b1.view(-1, 1, self.embed_dim)
         hidden = F.elu(th.bmm(agent_qs, w1) + b1)
         # Second layer
-        w_final = th.abs(self.hyper_w_final(states))
+        w_final = self.hyper_w_final(states)
         w_final = w_final.view(-1, self.embed_dim, 1)
         # State-dependent bias
         v = self.V(states).view(-1, 1, 1)
